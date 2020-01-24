ace41e893da2dc54e070e62e2868899106648efb
Chamika Ravinda <chamikaravinda@gmail.com>
2020-01-24 13:23:03 +0530

    add test

--- contracts/Election.sol
+++ contracts/Election.sol
@@ -1,11 +1,27 @@
-pragma solidity 0.4.2;
+pragma solidity ^0.5.16;
 
 contract Election {
-    // Read/write candidate
-    string public candidate;
-
-    // Constructor
-    function Election () public {
-        candidate = "Candidate 1";
+    // Model a Candidate
+    struct Candidate {
+        uint id;
+        string name;
+        uint voteCount;
     }
+
+    // Read/write candidates
+    mapping(uint => Candidate) public candidates;
+
+    // Store Candidates Count
+    uint public candidatesCount=0;
+
+    constructor () public {
+        addCandidate("Candidate 1");
+        addCandidate("Candidate 2");
+    }
+
+    function addCandidate (string memory _name) private {
+        candidatesCount ++;
+        candidates[candidatesCount] = Candidate(candidatesCount, _name, 0);
+    }
+
 }
